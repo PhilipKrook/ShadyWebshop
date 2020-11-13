6a0bcdbdfeedd784dd61f8b659c5c4e366b3fad3
Philip Krook <philip.krook@tietoevry.com>
2020-11-13 11:51:13 +0100

    Added MVC for ShoppingCart page

---
+++ LIAuppgift/Models/Pages/CartPage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace LIAuppgift.Models.Pages
+{
+    [ContentType(DisplayName = "CartPage", GUID = "4d2764dc-e387-4a7b-af1a-e6e98a104b10", Description = "")]
+    public class CartPage : PageData
+    {
+        /*
+                [CultureSpecific]
+                [Display(
+                    Name = "Main body",
+                    Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+                    GroupName = SystemTabNames.Content,
+                    Order = 1)]
+                public virtual XhtmlString MainBody { get; set; }
+         */
+    }
+}
