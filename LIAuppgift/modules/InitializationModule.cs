9161972729a279d2a49c5d5c9a2b6c2ab74cd65f
Philip Krook <philip.krook@tietoevry.com>
2021-02-18 16:32:24 +0100

    Epi + identity setup

---
+++ LIAuppgift/modules/InitializationModule.cs
@@ -0,0 +1,21 @@
+﻿using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using EPiServer.UI;
+using System;
+using System.Linq;
+
+namespace LIAuppgift.modules
+{
+    [EPiServer.Framework.InitializableModule]
+    [EPiServer.Framework.ModuleDependency(typeof(EPiServer.Cms.UI.AspNetIdentity.ApplicationSecurityEntityInitialization))]
+    [EPiServer.Framework.ModuleDependency(typeof(EPiServerUIInitialization))]
+    public class MyInitializationModule : IInitializableModule
+    {
+        public void ConfigureContainer(EPiServer.ServiceLocation.ServiceConfigurationContext context)
+        {
+            //Configure your providers
+        }
+        public void Initialize(EPiServer.Framework.Initialization.InitializationEngine context) { }
+        public void Uninitialize(EPiServer.Framework.Initialization.InitializationEngine context) { }        
+    }
+}
