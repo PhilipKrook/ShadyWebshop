ebc2a7209f1f1a2bb0423c11ffa74b2bce98caa4
Philip Krook <philip.krook@tietoevry.com>
2021-02-26 10:38:46 +0100

    Moved classes from CartPageController

---
+++ LIAuppgift/Models/Entities/CartItemEntity.cs
@@ -0,0 +1,22 @@
+﻿namespace LIAuppgift.Controllers
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class CartItemEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int Price { get; set; }
+
+        public int ProductId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int SumPrice { get; set; }
+
+        public string Name { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
