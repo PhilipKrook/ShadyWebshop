b37c4a731a9969bee4bf9339c285fa17cb56f066
Philip Krook <philip.krook@tietoevry.com>
2021-03-05 09:03:34 +0100

    gitignore is now working

--- LIAuppgift/CMSAdmin.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LIAuppgift
-{
-    public class CMSAdmin
-    {
-        public static string adminName = "ShAdYuSeR123";
-        public static string adminPw = "EpimvcUser1234!";
-        public static string adminEmail = "sHaDy@mvc.se";
-    }
-}
